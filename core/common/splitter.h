51d33a283d95d88564fa4b1c9ce808c22a976b0e
shinji3 <renrakusaki2003@yahoo.co.jp>
2017-08-04 13:18:47 +0900

    splitter.hの文字コードをshift_jisに変更

--- core/common/splitter.h
+++ core/common/splitter.h
@@ -3,8 +3,8 @@
 
 #include "stream.h"
 
-// ãƒ‡ãƒ¼ã‚¿ã‚¹ãƒˆãƒªãƒ¼ãƒ ã‚’è¤‡æ•°ã®ã‚¹ãƒˆãƒªãƒ¼ãƒ ã«åˆ†å²ã™ã‚‹ã€‚close æ™‚ã€ã‚ã‚‹ã„ã¯ãƒ‡
-// ã‚³ãƒ³ã‚¹ãƒˆãƒ©ã‚¯ãƒˆæ™‚ã«å‡ºåŠ›å…ˆã®ã‚¹ãƒˆãƒªãƒ¼ãƒ ã‚‚ close ã•ã‚Œã‚‹ã€‚
+// ƒf[ƒ^ƒXƒgƒŠ[ƒ€‚ğ•¡”‚ÌƒXƒgƒŠ[ƒ€‚É•ªŠò‚·‚éBclose A‚ ‚é‚¢‚Íƒf
+// ƒRƒ“ƒXƒgƒ‰ƒNƒg‚Éo—Íæ‚ÌƒXƒgƒŠ[ƒ€‚à close ‚³‚ê‚éB
 struct StreamSplitter : public Stream
 {
     StreamSplitter(const std::vector<Stream*>& streams = {})
